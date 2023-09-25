afea6a09213dc3d2e200f31ae31c322c797f615b
Lucas Moreira Hungaro <lhungaro10@gmail.com>
2023-09-25 16:41:48 -0300

    feat: function square

--- teste.js
+++ teste.js
@@ -5,3 +5,11 @@ function sum(a,b){
 function sub(a,b){
     return a -b;
 }
+
+function square(a){
+    if(a < 0){
+        console.log("Número inválido");
+    }else {
+        return Math.sqrt(a);
+    }
+}
